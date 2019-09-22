40ca1549e2771cab788972cd30400b472e4b4098
Tommy Cox <tommy.cox@protonmail.com>
2019-09-21 21:29:03 -0600

    MAO form

--- src/views/mao/Mao.js
+++ src/views/mao/Mao.js
@@ -1,47 +1,42 @@
 import React from "react";
 import { APIClient, Openlaw } from "openlaw";
 import OpenLawForm from "openlaw-elements";
+import MaoTemplate from "./MaoTemplate";
 
 // OpenLaw APIClient: https://docs.openlaw.io/api-client/#authentication
 //  - used to fetch geo data in our `Address` field type
 //  - run against your own private OpenLaw instance: 'https://[YOUR.INSTANCE.URL]';
 const apiClient = new APIClient("https://app.openlaw.io");
 // see tip below about authentication
-apiClient.login("tommy.cox@protonmail.com", "INSERT_PASSWORD_HERE");
+apiClient.login(process.env.OPENLAW_USER, process.env.OPENLAW_PASS);
 
-apiClient.getTemplate('LassoDAO').then(result => {
-  const { compiledTemplate } = Openlaw.compileTemplate(
-    result
-  );
-  const { executionResult, errorMessage } = Openlaw.execute(
-    compiledTemplate,
-    {},
-    {},
-    {}
-  );
-  this.setState({
-    executionResult,
-    variables: Openlaw.getExecutedVariables(executionResult, {}),
-    parameters: {}
-  });
+const { compiledTemplate } = Openlaw.compileTemplate(
+  MaoTemplate
+);
+const { executionResult, errorMessage } = Openlaw.execute(
+  compiledTemplate,
+  {},
+  {},
+  {}
+);
+const variables = Openlaw.getExecutedVariables(executionResult, {});
+const parameters = {};
 
-  if (errorMessage) {
-    console.error("Openlaw Execution Error:", errorMessage);
-  }
+if (errorMessage) {
+  console.error("Openlaw Execution Error:", errorMessage);
+}
 
-  // const onChange = (key, value, validationData) =>
-  //   console.log("KEY:", key, "VALUE:", value, "VALIDATION:", validationData);
-  
-})
+const onChange = (key, value, validationData) =>
+  console.log("KEY:", key, "VALUE:", value, "VALIDATION:", validationData);
 
 const Mao = () => (
   <OpenLawForm
     apiClient={apiClient}
-    executionResult={this.state.executionResult}
-    parameters={this.state.parameters}
-    // onChangeFunction={onChange}
+    executionResult={executionResult}
+    parameters={parameters}
+    onChangeFunction={onChange}
     openLaw={Openlaw}
-    variables={this.state.variables}
+    variables={variables}
   />
 );
 
