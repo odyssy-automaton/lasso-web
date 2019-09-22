33c71cb4f561e26bd378ed2a55faf9335e110281
skuhlmann <samkuhlmann@gmail.com>
2019-09-22 08:42:39 -0600

    hides footer on some routes

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,20 +1,31 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import ActivateButton from "../../components/activateButton/ActivateButton";
 
 import "./Footer.scss";
 import Brand from "../../assets/logo.png";
-const Footer = () => {
+const Footer = withRouter(({ ...props }) => {
+  const { location } = props;
+  console.log("location", location);
+
+  const route = location.pathname.split("/")[1];
+  const hide = route === "apply" || route === "summon";
+  if (hide) {
+    return null;
+  }
+
   return (
     <div className="Footer">
       <div className="Row">
         <Link to={`/`} className="Brand" href="/" alt="Lasso Home">
           <h1>Lasso</h1>
         </Link>
-        <a href='https://t.me/joinchat/IJqu9xOfqsnAzdHNGJz2EA'>Join the Telegram Group</a>
+        <a href="https://t.me/joinchat/IJqu9xOfqsnAzdHNGJz2EA">
+          Join the Telegram Group
+        </a>
       </div>
     </div>
   );
-};
+});
 
 export default Footer;
