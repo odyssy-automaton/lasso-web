32c0fe63603a50fb934031987e84cafee80bb1f4
Tommy Cox <tommy.cox@protonmail.com>
2019-09-21 21:44:49 -0600

    Move CreateMao form to its own component

--- src/views/mao/Mao.js
+++ src/views/mao/Mao.js
@@ -1,43 +1,6 @@
 import React from "react";
-import { APIClient, Openlaw } from "openlaw";
-import OpenLawForm from "openlaw-elements";
-import MaoTemplate from "./MaoTemplate";
+import CreateMao from "../../components/maoForms/CreateMao";
 
-// OpenLaw APIClient: https://docs.openlaw.io/api-client/#authentication
-//  - used to fetch geo data in our `Address` field type
-//  - run against your own private OpenLaw instance: 'https://[YOUR.INSTANCE.URL]';
-const apiClient = new APIClient("https://app.openlaw.io");
-// see tip below about authentication
-apiClient.login(process.env.OPENLAW_USER, process.env.OPENLAW_PASS);
-
-const { compiledTemplate } = Openlaw.compileTemplate(
-  MaoTemplate
-);
-const { executionResult, errorMessage } = Openlaw.execute(
-  compiledTemplate,
-  {},
-  {},
-  {}
-);
-const variables = Openlaw.getExecutedVariables(executionResult, {});
-const parameters = {};
-
-if (errorMessage) {
-  console.error("Openlaw Execution Error:", errorMessage);
-}
-
-const onChange = (key, value, validationData) =>
-  console.log("KEY:", key, "VALUE:", value, "VALIDATION:", validationData);
-
-const Mao = () => (
-  <OpenLawForm
-    apiClient={apiClient}
-    executionResult={executionResult}
-    parameters={parameters}
-    onChangeFunction={onChange}
-    openLaw={Openlaw}
-    variables={variables}
-  />
-);
+const Mao = () => <CreateMao />;
 
 export default Mao;
