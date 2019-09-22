42bc80cf21843ffd7d6c2485f4e41473113ab31a
Tommy Cox <tommy.cox@protonmail.com>
2019-09-22 03:24:30 -0600

    TODO: Grab params and upload draft from those params

--- src/components/moaForm/CreateMoa.js
+++ src/components/moaForm/CreateMoa.js
@@ -40,6 +40,29 @@ const signAgreement = async (context, props) => {
     "Please sign the Operating Agreement",
     context.account
   );
+
+  // TODO: Grab params and upload draft from those params.
+  // const params = {
+  //   templateId:
+  //     "29f529e7f819fa2beb1c4a8bf258a15cfe46dad4f91538ebedbd1fb7299bbc55",
+  //   title: "Advisor Agreement",
+  //   text:
+  //     'This Advisor Agreement is entered into between [[Company Name: Text]] ("Corporation") and [[Advisor Name]] ("Advisor") as of [[Effective Date: Date]] ("Effective Date"). Company and Advisor agree as follows:  \n\n^ **Services**. Advisor agrees to consult with and advise Company from time to time, at Company\'s request (the "Services"). {{No Services "Do you want to limit the advisor\'s services?"  While this Agreement is is effect, Advisor will not provide services to any company active in the field of [[Noncompete Field "What field should the advisor not participate in?"]].}}\n\n**COMPANY:**\n[[Company Signatory Email: Identity | Signature]]\n\n___________________\nName: [[Company Signatory]]\nAddress: [[Company Address: Address]]\n\n\n**ADVISOR:**\n[[Advisor Email: Identity | Signature]]\n\n___________________\nName: [[Advisor Name]]\nAddress: [[Advisor Address: Address]]\n',
+  //   creator: "8f26427b-0853-469b-a4f1-132190b7373e",
+  //   parameters: {
+  //     "Company Name": "ABC, Inc.",
+  //     "Company Signatory Email":
+  //       '{"id":{"id":"8f26427b-0853-469b-a4f1-132190b7373e"},"email":"openlawuser+1@gmail.com","identifiers":[{"identityProviderId":"openlaw","identifier":"openlawuser+1@gmail.com"}]}',
+  //     "Advisor Email":
+  //       '{"id":{"id":"38e0eb6b-0d52-4fd8-a77d-19686fd3843a"},"email":"openlawuser+2@gmail.com","identifiers":[{"identityProviderId":"openlaw","identifier":"openlawuser+2@gmail.com"}]}'
+  //   },
+  //   overriddenParagraphs: {},
+  //   agreements: {},
+  //   readonlyEmails: [],
+  //   editEmails: [],
+  //   draftId: ""
+  // };
+  // apiClient.uploadDraft(params);
   props.history.push(`/dao/${props.address}`);
 };
 
